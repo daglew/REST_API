a3d62c9225d6bd99168187dd872b848d8b237da7
daglew <daga122611@gmail.com>
2024-06-02 23:07:50 +0200

    put_request

--- request_examples/PUT_request.py
+++ request_examples/PUT_request.py
@@ -3,23 +3,18 @@ import json
 import jsonpath
 import requests
 
-api_url = "https://reqres.in/api/users"
+api_url = "https://reqres.in/api/users/2"
 
 
 file = open('C:\\Users\\dagle\\OneDrive\\Dokumenty\\API\\user_create.json', 'r')
-
 input_json = file.read()
 json_requests = json.loads(input_json)
 
-response = requests.post(api_url, json_requests)
+response = requests.put(api_url, json_requests)
 
-assert response.status_code == 201
-
-print(response.headers.get('Content-Length'))
+assert response.status_code == 200
 
 response_json = json.loads(response.text)
+updated = jsonpath.jsonpath(response_json, 'updatedAt')
 
-id = jsonpath.jsonpath(response_json, 'id')
-
-print(id[0])
-
+print(updated[0])
