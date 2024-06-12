76ca09fd859757e24a3d39c49168d679b85d6c1f
daglew <daga122611@gmail.com>
2024-06-12 23:02:28 +0200

    TestCase_add_student

--- TestCase/test_add_new_student.py
+++ TestCase/test_add_new_student.py
@@ -3,7 +3,7 @@ import jsonpath
 import requests
 
 
-def test_add_student():
+def test_add_data_student():
     api_url = 'https://thetestingworldapi.com/'
     file = open('C:\\Users\\dagle\\OneDrive\\Dokumenty\\API\\create_new_student_json', 'r')
     json_request = json.loads(file.read())
@@ -11,4 +11,10 @@ def test_add_student():
     print(response.text)
 
 
+def test_get_data_student():
+    api_url = 'https://thetestingworldapi.com/3034'
+    response = requests.get(api_url)
+    response_json = response.json()
+    id = jsonpath.jsonpath(response_json, 'data.id')
+    assert id[0] == 3034
 
