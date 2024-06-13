2e7387d2fa1337d78b750a7fcf677d0064037256
daglew <daga122611@gmail.com>
2024-06-13 22:58:53 +0200

    TestCase_put_student

--- TestCase/test_add_new_student.py
+++ TestCase/test_add_new_student.py
@@ -4,17 +4,26 @@ import requests
 
 
 def test_add_data_student():
-    api_url = 'https://thetestingworldapi.com/'
-    file = open('C:\\Users\\dagle\\OneDrive\\Dokumenty\\API\\create_new_student_json', 'r')
+    api_url = 'https://thetestingworldapi.com/api/studentsDetails'
+    file = open('C:\\Users\\dagle\\OneDrive\\Dokumenty\\API\\create_new_student.json', 'r')
     json_request = json.loads(file.read())
-    response = requests.get(api_url, json_request)
+    response = requests.post(api_url, json_request)
+    print(response.text)
+
+
+def test_update_data_student():
+    api_url = 'https://thetestingworldapi.com/api/studentsDetails/10289292'
+    file = open('C:\\Users\\dagle\\OneDrive\\Dokumenty\\API\\create_new_student.json', 'r')
+    json_request = json.loads(file.read())
+    response = requests.post(api_url, json_request)
     print(response.text)
 
 
 def test_get_data_student():
-    api_url = 'https://thetestingworldapi.com/3034'
+    api_url = 'https://thetestingworldapi.com/api/studentsDetails/10289292'
     response = requests.get(api_url)
     response_json = response.json()
+    print(response_json)
     id = jsonpath.jsonpath(response_json, 'data.id')
-    assert id[0] == 3034
+    assert id[0] == 10289292
 
