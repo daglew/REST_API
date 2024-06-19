2354351d540ab08c0fd69dc06538ff1f66a70114
daglew <daga122611@gmail.com>
2024-06-19 23:12:00 +0200

    TestCase_chaining_request

---
+++ TestCase/test_chaining_request.py
@@ -0,0 +1,21 @@
+import json
+import jsonpath
+import requests
+
+
+def new_student_add():
+    api_url = "https://thetestingworldapi.com/api/studentsDetails"
+    file = open('C:/Users/dagle/OneDrive/Dokumenty/API/student_add.json')
+    json_request = json.loads(file.read())
+    response = requests.post(api_url, json_request)
+    id = jsonpath.jsonpath((response.json(), 'id'))
+    print(id[0])
+
+
+def test_details_get():
+    api_url = "https://thetestingworldapi.com/api/studentsDetails"+str(id[0])
+    response = requests.get(api_url)
+    print(response.text)
+
+
+
