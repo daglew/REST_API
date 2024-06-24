09c957c306b3e62eb0024124f9f4c00406631e7f
daglew <daga122611@gmail.com>
2024-06-24 22:15:31 +0200

    TestCase_test_oauth_api_2

--- TestCase/test_oauth_authentication.py
+++ TestCase/test_oauth_authentication.py
@@ -9,12 +9,8 @@ def test_oath():
     response = requests.post(url_token, data)
     value_token = jsonpath.jsonpath(response.json(), 'access_token')
 
-    auth = {'Authorization': 'Bearer' + value_token}
+    auth = {'Authorization': 'Bearer' + str(value_token)}
     api_url = 'http://thetestingworldapi.com/api/stdetails/10290055'
     response = requests.get(api_url, headers=auth)
     print(response.text)
-
-
-
-
 
