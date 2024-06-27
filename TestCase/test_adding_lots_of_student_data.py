7eb9e474d2cebb203777ea1448ac04089d3f7f1a
daglew <daga122611@gmail.com>
2024-06-27 23:00:01 +0200

    test_adding_lots_of_student_data2

--- TestCase/test_adding_lots_of_student_data.py
+++ TestCase/test_adding_lots_of_student_data.py
@@ -9,6 +9,25 @@ def test_adding_lots_of_student_data():
     file = open('C:\\Users\\dagle\\OneDrive\\Dokumenty\\API\\create_new_student.json')
     json_request = json.loads(file.read())
 
-    wk = openpyxl.load_workbook(('C:\\Users\\dagle\\OneDrive\\Dokumenty\\API\\')
+    wk = openpyxl.load_workbook('C:\\Users\\dagle\\OneDrive\\Dokumenty\\API\\table2')
+    sh = wk['Sheet1']
+    rows = sh.max_row
 
-    
+    for i in range(2, rows+1):
+        first_name_cell = sh.cell(rov=i, column=1)
+        midd_name_cell = sh.cell(rov=i, column=2)
+        last_name_cell = sh.cell(rov=i, column=3)
+        data_cell = sh.cell(rov=i, column=4)
+
+        json_request['first_name'] = first_name_cell.value
+        json_request['midd_name_cell'] = midd_name_cell.value
+        json_request['last_name_cell'] = last_name_cell.value
+        json_request['data_cell'] = data_cell.value
+
+        response = requests.post(api_url, json_request)
+        print(response.text)
+        print(response.status_code)
+        assert response.status_code == 201
+
+
+
